73587739a53e29f18ba22879291b38afef57a4f0
Tigran <41443306+septoon@users.noreply.github.com>
2024-01-14 13:39:35 +0300

    added nav routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header"
+import Menu from "./components/Menu/Menu";
+import Gallery from "./components/Gallery/Gallery";
+import About from "./components/About/About";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <div className="bg-shd bg-cover bg-center h-screen">
       <Header />
+      <Routes>
+        <Route exact path="/" element={<Menu />} />
+        <Route exact path="/gallery" element={<Gallery />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className="container flex h-20 items-center">
-      <div className='flex flex-row '>
-        <h3 className="text-3xl md:text-2xl sm:text-4xl font-bold uppercase">Шашлычный дом</h3>
+    <div className="container flex h-20 items-center justify-between">
+      <div className='flex flex-row justify-center w-1/3'>
+        <h3 className="text-base font-bold uppercase">Шашлычный дом</h3>
+      </div>
+      <div className='w-2/3 flex justify-around'>
+        <NavLink to="/">Меню</NavLink>
+        <NavLink to="/gallery">Галерея</NavLink>
+        <NavLink to="/about">О нас</NavLink>
+        <NavLink to="/cart">Корзина</NavLink>
       </div>
     </div>
   )
