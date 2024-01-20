3450d2b3faebad429ca2ec78cb5ba69ce6629a4b
Tigran <41443306+septoon@users.noreply.github.com>
2024-01-20 16:29:08 +0300

    update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Main = () => {
   return (
     <div className="pt-6 px-6 w-full flex flex-col justify-center h-[calc(100vh-80px)] overflow-y-hidden bg-main text-white bg-cover bg-no-repeat bg-center bg-fixed">
-      <div className='flex flex-col justify-between h-[75%] mt-8'>
+      <div className='flex flex-col justify-around h-full mt-8'>
         <div className=" flex flex-col">
           <span className='text-xl italic'>Доставка вкусного шашлыка в Алуште</span>
           <span className='text-xl font-semibold my-3'>Бесплатная доставка от 1000 рублей</span>
@@ -24,13 +24,14 @@ const Main = () => {
           <span className='text-xl shadow'>Стоимость доставки:</span>
           <span className='text-xl font-semibold'>200р</span>
         </div>
-      </div>
         <div className="flex flex-col w-auto">
           <span className='text-xl'>Заказать можно по номеру:</span>
           <span className="font-extrabold text-white text-xl">
             <a href="tel:+79788796220">+7 (978) 879-62-20</a>
           </span>
         </div>
+      </div>
+        
     </div>
   );
 };
