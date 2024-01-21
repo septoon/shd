a7ed756d82231984cbe0e1b9b5d5ab90b171e596
Tigran <41443306+septoon@users.noreply.github.com>
2024-01-21 21:48:01 +0300

    added contacts

--- src/components/About/Contacts.jsx
+++ src/components/About/Contacts.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import PhoneIcon from '../../assets/img/phone.svg'
+import AddressIcon from '../../assets/img/location.svg'
+import ClockIcon from '../../assets/img/clock.svg'
 
 const Contacts = () => {
   return (
-    <div className='pt-6 w-full'>
-      <h1 className="pl-6 text-title font-bold font-comfortaa">Контакты</h1>
+    <div className='pt-6 w-full flex flex-col'>
+      <h1 className="pl-6 mb-6 text-title font-bold font-comfortaa">Контакты</h1>
+
+      <div className='w-full flex flex-col pl-3'>
+        <div className='flex items-center mb-3'>
+          <img src={PhoneIcon} className='w-8 mr-4' alt="phone" />
+          <div className='flex flex-col'>
+            <span>Телефон</span>
+            <span className='font-bold'><a href="tel:+79788796220">+ 7 (978) 879-62-20</a></span>
+          </div>
+        </div>
+
+        <div className='flex items-center mb-3'>
+          <img src={AddressIcon} className='w-8 mr-4' alt="address" />
+          <div className='flex flex-col'>
+            <span>Адрес</span>
+            <span className='font-bold'>г. Алушта, ул. Ленина 13</span>
+          </div>
+        </div>
+
+        <div className='flex items-center mb-3'>
+          <img src={ClockIcon} className='w-8 mr-4' alt="clock" />
+          <div className='flex flex-col'>
+            <span>Режим работы</span>
+            <span className='font-bold'>11:00 - 23:00</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
