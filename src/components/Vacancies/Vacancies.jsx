56f6aea667e39a3541396fa99a931ab0142a68e7
Tigran <41443306+septoon@users.noreply.github.com>
2024-01-25 22:18:13 +0300

    update

--- src/components/Vacancies/Vacancies.jsx
+++ src/components/Vacancies/Vacancies.jsx
@@ -4,7 +4,9 @@ const Vacancies = () => {
   return (
     <div className='pt-6 w-full h-full flex flex-col'>
       <h1 className="pl-6 mb-6 text-title font-bold font-comfortaa">Вакансии</h1>
-      
+      <div className='pl-3'>
+        <span>В данный момент нет свободных вакансий</span>
+      </div>
     </div>
   )
 }
