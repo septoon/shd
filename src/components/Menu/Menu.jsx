a0a53dec6ac4a173db2a9297b3e079a03d6f88da
Tigran <41443306+septoon@users.noreply.github.com>
2024-01-27 20:54:45 +0300

    added vacancies

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -22,9 +22,18 @@ const Menu = ({ item, catalogName }) => {
 
   return (
     <div className="w-full flex flex-col justify-start mt-6">
-      <h3 className="px-5 text-title font-extrabold font-comfortaa text-dark mb-8">
+      <h3 className="px-5 text-title font-extrabold font-comfortaa text-dark mb-3">
         {catalogName ? catalogName : 'Холодные закуски'}
       </h3>
+      {
+        catalogName === 'Блюда на мангале' && (
+          <div className="px-5 text-[14px] py-2 w-auto rounded-md font-extrabold font-comfortaa text-dark mb-8">
+            <span className="px-2 text-[14px] border-[1px] border-dashed w-auto py-2 rounded-md font-extrabold font-comfortaa text-dark mb-8">
+              Минимальный заказ от 300 грамм
+            </span>
+          </div>
+        )
+      }
       <div className=" flex flex-wrap justify-start px-5 pb-[80px]">
         {item.map((i) => (
           <MenuItem
--- src/components/Vacancies/Vacancies.jsx
+++ src/components/Vacancies/Vacancies.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
+import WaiterIcon from '../../assets/img/icons/Waiter.png'
+import CookIcon from '../../assets/img/icons/cook.png'
+import MangalIcon from '../../assets/img/icons/mangal.png'
+import AdministratorIcon from '../../assets/img/icons/Administrator.png'
+import WashingIcon from '../../assets/img/icons/Washing.png'
 
 const Vacancies = () => {
   return (
     <div className='pt-6 w-full h-full flex flex-col'>
       <h1 className="pl-6 mb-6 text-title font-bold font-comfortaa">Вакансии</h1>
-      <div className='pl-3'>
-        <span>В данный момент нет свободных вакансий</span>
+      <div className='pl-3 mb-8'>
+        <div className='flex items-center mb-2'>
+          <img className='w-10 mr-3' src={WaiterIcon} alt="WaiterIcon" />
+          <span className='text-xl font-comfortaa'>Официант</span>
+        </div>
+        <div className='flex items-center mb-2'>
+          <img className='w-10 mr-3' src={CookIcon} alt="CookIcon" />
+          <span className='text-xl font-comfortaa'>Повар</span>
+        </div>
+        <div className='flex items-center mb-2'>
+          <img className='w-10 mr-3' src={MangalIcon} alt="MangalIcon" />
+          <span className='text-xl font-comfortaa'>Мангальщик</span>
+        </div>
+        <div className='flex items-center mb-2'>
+          <img className='w-10 mr-3' src={AdministratorIcon} alt="AdministratorIcon" />
+          <span className='text-xl font-comfortaa'>Администратор</span>
+        </div>
+        <div className='flex items-center mb-2'>
+          <img className='w-10 mr-3' src={WashingIcon} alt="WashingIcon" />
+          <span className='text-xl font-comfortaa'>Посудомойщица</span>
+        </div>
       </div>
+      <span className='pl-3 mb-2'>Обращаться по телефону:</span>
+      <span className=" pl-3 font-extrabold text-lightSlate-gray text-xl">
+        <a href="tel:+79788796220">+7 (978) 879-62-20</a>
+      </span>
     </div>
   )
 }
